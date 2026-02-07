67d156fa5114d8b99b35864441544ad92d0d1de0
Cauê da Rocha Garcia <cauerg7@gmail.com>
2026-02-07 14:10:26 -0300

    refactor: register method returns 201, authController with Swagger

--- src/main/java/com/security/test/api/controller/AuthController.java
+++ src/main/java/com/security/test/api/controller/AuthController.java
@@ -5,13 +5,19 @@ import com.security.test.api.dto.request.RegisterRequestDTO;
 import com.security.test.api.dto.response.RegisterResponseDTO;
 import com.security.test.api.dto.response.LoginResponseDTO;
 import com.security.test.api.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/auth")
@@ -23,17 +29,35 @@ public class AuthController {
         this.service = service;
     }
 
-    @PostMapping(value = "/login")
+    @Operation(
+            description = "Endpoint for login",
+            summary = "Login credentials",
+            responses = {
+                    @ApiResponse(description = "Ok", responseCode = "200"),
+                    @ApiResponse(description = "Unprocessable Entity", responseCode = "422")
+            }
+    )
+    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("permitAll()")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginRequestDTO request) {
         var response =  service.login(request);
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("/register")
+    @Operation(
+            description = "Endpoint for register new user",
+            summary = "Register new user",
+            responses = {
+                    @ApiResponse(description = "Ok", responseCode = "201"),
+                    @ApiResponse(description = "Unprocessable Entity", responseCode = "422")
+            }
+    )
+    @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("permitAll()")
-    public ResponseEntity<RegisterResponseDTO> register(@RequestBody @Valid RegisterRequestDTO data) {
-        var newUser = service.register(data);
-        return ResponseEntity.ok(newUser);
+    public ResponseEntity<RegisterResponseDTO> register(@RequestBody @Valid RegisterRequestDTO request) {
+        var newUser = service.register(request);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(newUser.id()).toUri();
+        return ResponseEntity.created(uri).body(newUser);
     }
 }
